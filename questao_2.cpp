331188678e0f9ac544e7f848cdc6e9c2face8755
IFPB <ifpb@MDK00LB10-JP>
2019-08-30 21:31:26 -0300

    Foi respondido a questão 2

---
+++ questao_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    cout << "Tamanho do boolean: " << sizeof(bool)<< endl;
+    cout << "Tamanho do inteiro: " << sizeof(int)<< endl;
+    cout << "Tamanho do double: " << sizeof(double)<< endl;
+    cout << "Tamanho do float: " << sizeof(float)<< endl;
+    cout << "Tamanho do char: " << sizeof(char)<< endl;
+    cout << "Tamanho do long: " << sizeof(long)<< endl;
+    cout << "Tamanho do short: " << sizeof(short)<< endl;
+
+
+}
