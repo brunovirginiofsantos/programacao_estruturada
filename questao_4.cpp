b572f1072a5e3d3ad00a3120b110c13e742d1369
Bruno santos <bruno.vfsantos@gmail.com>
2019-09-06 21:21:43 -0300

    Questão 4 foi respondida

--- questao_4.cpp
+++ questao_4.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 int main (){
 
+    string texto;
+    ifstream ler_texto;
     
-
-
+    ler_texto.open("texto.txt");
+    while(getline(ler_texto,texto)){
+        cout << texto << endl;
+    }
+        
+    ler_texto.close();
 }
